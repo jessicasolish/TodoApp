d13d7bc8b7c9621d5411fcbfad7ddf7d1383f012
jessicasolish <jessica.solis.h@gmail.com>
2016-07-06 23:55:18 -0300

    continuacion del trabajo

--- js/main.js
+++ js/main.js
@@ -6,3 +6,34 @@ var lista = document.getElementById("lista");
 
 
 
+
+//FUNCIONES
+function elminarItem(evento){
+    console.log("elminarItem");
+    var li = evento.target.parentNode;
+    lista.removeChild(li);
+} 
+
+function agregarTexto(){
+    var letras = texto.value ;
+    var li = document.createElement("li");
+    var p = document.createTextNode( letras );
+    var button = document.createElement("button");
+    if(letras == ""){
+        texto.setAttribute("placeholder"," Añade Una Tarea,no dejes en Blanco!!!");
+    }else{
+        button.setAttribute("class","btn-danger");
+        button.addEventListener("click", elminarItem);
+        var buttonTexto = document.createTextNode('x');
+        button.appendChild(buttonTexto);
+        li.appendChild(p);
+        li.appendChild(button);
+        lista.appendChild(li);
+        }
+    
+}
+
+function eliminarTodo(){
+    lista.innerHTML = "";
+}
+botonTexto.addEventListener("click", agregarTexto );
